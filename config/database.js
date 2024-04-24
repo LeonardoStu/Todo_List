0a74d1cdce5d00dc297d8667d9d7fbf9be00be01
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-04-24 12:02:53 -0300

    Instalando o mongoose

---
+++ config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/todo-list')
+    .then(() => console.log('Conectado ao mongoDB'))
+    .catch(err => console.log(err))
